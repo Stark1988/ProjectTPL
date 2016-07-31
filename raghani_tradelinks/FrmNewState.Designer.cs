9093b5e5c932de4aac89443378c6cff4ae33942b
Stark1988 <kavin.shah.88@gmail.com>
2016-07-31 16:42:45 +0530

    city state form validations

--- raghani_tradelinks/FrmNewState.Designer.cs
+++ raghani_tradelinks/FrmNewState.Designer.cs
@@ -28,54 +28,55 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
+            DevExpress.XtraEditors.DXErrorProvider.ConditionValidationRule conditionValidationRule1 = new DevExpress.XtraEditors.DXErrorProvider.ConditionValidationRule();
             this.panelControl1 = new DevExpress.XtraEditors.PanelControl();
             this.btnCancel = new DevExpress.XtraEditors.SimpleButton();
-            this.btnSave = new DevExpress.XtraEditors.SimpleButton();
-            this.txtStateName = new System.Windows.Forms.TextBox();
+            this.btnEdit = new DevExpress.XtraEditors.SimpleButton();
             this.lblStateName = new DevExpress.XtraEditors.LabelControl();
             this.lblTitle = new DevExpress.XtraEditors.LabelControl();
+            this.btnAdd = new DevExpress.XtraEditors.SimpleButton();
+            this.btnDelete = new DevExpress.XtraEditors.SimpleButton();
+            this.dxValidationProvider1 = new DevExpress.XtraEditors.DXErrorProvider.DXValidationProvider(this.components);
+            this.txtStateName = new DevExpress.XtraEditors.TextEdit();
             ((System.ComponentModel.ISupportInitialize)(this.panelControl1)).BeginInit();
             this.panelControl1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dxValidationProvider1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.txtStateName.Properties)).BeginInit();
             this.SuspendLayout();
             // 
             // panelControl1
             // 
-            this.panelControl1.Controls.Add(this.btnCancel);
-            this.panelControl1.Controls.Add(this.btnSave);
             this.panelControl1.Controls.Add(this.txtStateName);
+            this.panelControl1.Controls.Add(this.btnDelete);
+            this.panelControl1.Controls.Add(this.btnAdd);
+            this.panelControl1.Controls.Add(this.btnCancel);
+            this.panelControl1.Controls.Add(this.btnEdit);
             this.panelControl1.Controls.Add(this.lblStateName);
             this.panelControl1.Location = new System.Drawing.Point(12, 41);
             this.panelControl1.Name = "panelControl1";
-            this.panelControl1.Size = new System.Drawing.Size(311, 120);
+            this.panelControl1.Size = new System.Drawing.Size(332, 120);
             this.panelControl1.TabIndex = 18;
             // 
             // btnCancel
             // 
             this.btnCancel.DialogResult = System.Windows.Forms.DialogResult.Cancel;
-            this.btnCancel.Location = new System.Drawing.Point(101, 61);
+            this.btnCancel.Location = new System.Drawing.Point(251, 61);
             this.btnCancel.Margin = new System.Windows.Forms.Padding(3, 1, 3, 1);
             this.btnCancel.Name = "btnCancel";
             this.btnCancel.Size = new System.Drawing.Size(75, 25);
             this.btnCancel.TabIndex = 25;
             this.btnCancel.Text = "Cancel";
             // 
-            // btnSave
+            // btnEdit
             // 
-            this.btnSave.Location = new System.Drawing.Point(8, 61);
-            this.btnSave.Margin = new System.Windows.Forms.Padding(3, 1, 3, 1);
-            this.btnSave.Name = "btnSave";
-            this.btnSave.Size = new System.Drawing.Size(75, 25);
-            this.btnSave.TabIndex = 24;
-            this.btnSave.Text = "Save";
-            // 
-            // txtStateName
-            // 
-            this.txtStateName.Location = new System.Drawing.Point(79, 12);
-            this.txtStateName.Margin = new System.Windows.Forms.Padding(3, 1, 3, 1);
-            this.txtStateName.MaxLength = 20;
-            this.txtStateName.Name = "txtStateName";
-            this.txtStateName.Size = new System.Drawing.Size(182, 21);
-            this.txtStateName.TabIndex = 19;
+            this.btnEdit.Location = new System.Drawing.Point(89, 61);
+            this.btnEdit.Margin = new System.Windows.Forms.Padding(3, 1, 3, 1);
+            this.btnEdit.Name = "btnEdit";
+            this.btnEdit.Size = new System.Drawing.Size(75, 25);
+            this.btnEdit.TabIndex = 24;
+            this.btnEdit.Text = "Edit";
+            this.btnEdit.Click += new System.EventHandler(this.btnEdit_Click);
             // 
             // lblStateName
             // 
@@ -98,6 +99,36 @@
             this.lblTitle.TabIndex = 26;
             this.lblTitle.Text = "State Maintainance";
             // 
+            // btnAdd
+            // 
+            this.btnAdd.Location = new System.Drawing.Point(8, 61);
+            this.btnAdd.Margin = new System.Windows.Forms.Padding(3, 1, 3, 1);
+            this.btnAdd.Name = "btnAdd";
+            this.btnAdd.Size = new System.Drawing.Size(75, 25);
+            this.btnAdd.TabIndex = 26;
+            this.btnAdd.Text = "Add";
+            this.btnAdd.Click += new System.EventHandler(this.btnAdd_Click);
+            // 
+            // btnDelete
+            // 
+            this.btnDelete.Location = new System.Drawing.Point(170, 61);
+            this.btnDelete.Margin = new System.Windows.Forms.Padding(3, 1, 3, 1);
+            this.btnDelete.Name = "btnDelete";
+            this.btnDelete.Size = new System.Drawing.Size(75, 25);
+            this.btnDelete.TabIndex = 27;
+            this.btnDelete.Text = "Delete";
+            // 
+            // txtStateName
+            // 
+            this.txtStateName.Location = new System.Drawing.Point(89, 12);
+            this.txtStateName.Name = "txtStateName";
+            this.txtStateName.Properties.MaxLength = 20;
+            this.txtStateName.Size = new System.Drawing.Size(172, 20);
+            this.txtStateName.TabIndex = 28;
+            conditionValidationRule1.ConditionOperator = DevExpress.XtraEditors.DXErrorProvider.ConditionOperator.IsNotBlank;
+            conditionValidationRule1.ErrorText = "State Name can not be blank.";
+            this.dxValidationProvider1.SetValidationRule(this.txtStateName, conditionValidationRule1);
+            // 
             // FrmNewState
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
@@ -114,6 +145,8 @@
             ((System.ComponentModel.ISupportInitialize)(this.panelControl1)).EndInit();
             this.panelControl1.ResumeLayout(false);
             this.panelControl1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dxValidationProvider1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.txtStateName.Properties)).EndInit();
             this.ResumeLayout(false);
 
         }
@@ -122,10 +155,13 @@
 
         private DevExpress.XtraEditors.PanelControl panelControl1;
         private DevExpress.XtraEditors.SimpleButton btnCancel;
-        private DevExpress.XtraEditors.SimpleButton btnSave;
-        private System.Windows.Forms.TextBox txtStateName;
+        private DevExpress.XtraEditors.SimpleButton btnEdit;
         private DevExpress.XtraEditors.LabelControl lblStateName;
         private DevExpress.XtraEditors.LabelControl lblTitle;
+        private DevExpress.XtraEditors.SimpleButton btnDelete;
+        private DevExpress.XtraEditors.SimpleButton btnAdd;
+        private DevExpress.XtraEditors.DXErrorProvider.DXValidationProvider dxValidationProvider1;
+        private DevExpress.XtraEditors.TextEdit txtStateName;
 
     }
 }
