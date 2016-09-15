249bb5129089f6ae294b9bef4fe7a048e3fc1fdd
J1990 <jayasolkar@gmail.com>
2016-09-15 18:15:23 +0530

    Design for Supplier Outstanding Report

---
+++ RT.BL/SupplierOutstandingReport.cs
@@ -0,0 +1,94 @@
+﻿using DevExpress.DataAccess.ObjectBinding;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RT.BL
+{
+    [HighlightedClass]
+    public class SupplierOutstandingReportData
+    {
+        public List<CustomerSOReport> CustomerData { get; set; }
+    }
+
+    public class CustomerSOReport
+    {
+        public string CustomerName { get; set; }
+        public DateTime? Date { get; set; }
+        public string BillNo { get; set; }
+
+        private decimal? billAmt;
+        public decimal? BillAmt
+        {
+            get
+            {
+                return billAmt;
+            }
+            set
+            {
+                if (value == null)
+                    billAmt = 0;
+                else
+                    billAmt = value;
+            }
+        }
+
+        private decimal? totalCommission;
+        public decimal? TotalCommission
+        {
+            get
+            {
+                return totalCommission;
+            }
+            set
+            {
+                if (value == null)
+                    totalCommission = 0;
+                else
+                    totalCommission = value;
+            }
+        }
+
+        private decimal? discount;
+        public decimal? Discount
+        {
+            get
+            {
+                return discount;
+            }
+            set
+            {
+                if (value == null)
+                    discount = 0;
+                else
+                    discount = value;
+            }
+        }
+
+        private decimal? receivedCommission;
+        public decimal? ReceivedCommission
+        {
+            get
+            {
+                return receivedCommission;
+            }
+            set
+            {
+                if (value == null)
+                    receivedCommission = 0;
+                else
+                    receivedCommission = value;
+            }
+        }
+
+        public decimal? Balance
+        {
+            get
+            {
+                return TotalCommission - (ReceivedCommission + Discount);
+            }
+        }
+    }
+}
